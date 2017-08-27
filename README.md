36bc49a19fa406726db0175bb978b98e5d22dd7d
zoltansallay <zoltan.sallay@codecool.com>
2017-08-27 09:57:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java SE: Interface and abstract class, method overriding
+
+Java SE module 3rd SI week, UML exercise
---
+++ src/com/codecool/uml/overriding/Webshop.java
@@ -0,0 +1,8 @@
+package com.codecool.uml.overriding;
+
+public class Webshop {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
